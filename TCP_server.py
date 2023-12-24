a99988efb3115fd9596471ee21f6a43ef615e622
mesz <h1@gamil.com>
2023-12-25 01:33:31 +0300

    update tcp server

--- TCP_server.py
+++ TCP_server.py
@@ -3,29 +3,31 @@ import requests
 import json
 
 
-# Replace 'YOUR_ACCESS_KEY' with your actual aviationstack API key
+# access code for API
 access_key = 'a5a5423540790ed7ad4db33bf14d26e9'
 
-# Replace 'YOUR_AIRPORT_CODE' with the airport code you are interested in
+# name of arr_icao
 arr_icao=input("Please enter airport code: ")
 
-# Aviationstack API endpoint for flights at a specific airport
+# API_URL and PARAMS
 api_url ='http://api.aviationstack.com/v1/flights'
 params_i = {
     'access_key': access_key,
     'arr_icao': arr_icao,
     'limit': 100  
 }
-# Make a GET request to the aviationstack API
+#THE RESPONSE AND CONVERT TO JSON
 response = requests.get(api_url , params=params_i )
 
 data = response.json()
 
-# Store the retrieved data in a JSON file
+#STORE IN JSON FILE WITH NAME GA6.JSON 
 with open('GA6.json', 'w') as f:
         json.dump(data , f , indent=2)
 
         print("Data saved to GA6.json")
+
+
 
 
 #creating a TCP socket :
@@ -38,11 +40,11 @@ with open('GA6.json', 'w') as f:
 
 # listen for 3 client :
 #ss.listen(3)
-
+    
 #accepting the connection
 #sss, sockname = ss.accept()
 
-# print out the returned values from accept() method
+#PRINT THE ACCEPTED METHOD
 #print('Accepted request from', sockname[0] ,'with port number', sockname[1])
 
 #closing the socket :

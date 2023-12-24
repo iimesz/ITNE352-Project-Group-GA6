467fa9f9fa97245df3677baaaee6e126ec857ee9
mesz <h1@gamil.com>
2023-12-24 23:31:15 +0300

    connection server and client

--- TCP_client.py
+++ TCP_client.py
@@ -1,1 +1,14 @@
 import socket
+
+
+#creating the socket:
+sc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect to server :
+adress ='127.0.0.2'
+port = 50000
+sc.connect((adress, port))
+print("The request has been sent")
+
+#closing the socket :
+sc.close()
--- TCP_server.py
+++ TCP_server.py
@@ -1,1 +1,25 @@
-import socket 
+import socket
+
+#creating a TCP socket :
+ss = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# binding the socket : 
+adress ='127.0.0.2'
+port = 50000
+ss.bind((adress, port))
+
+
+
+
+# listen for 3 client :
+ss.listen(3)
+
+#accepting the connection
+sss, sockname = ss.accept()
+
+# print out the returned values from accept() method
+print('Accepted request from', sockname[0] ,'with port number', sockname[1])
+
+#closing the socket :
+ss.close()
+sss.close()

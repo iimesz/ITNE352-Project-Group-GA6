6eb3a6cd8425e575669ebcef70219506a1b56724
mesz <h1@gamil.com>
2023-12-25 22:16:39 +0300

    update the server and client

--- TCP_client.py
+++ TCP_client.py
@@ -1,14 +1,33 @@
 import socket
 
 
-#creating the socket:
-sc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#connect to server :
-adress ='127.0.0.2'
+sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = '127.0.0.2'
 port = 50000
-sc.connect((adress, port))
-print("The request has been sent")
 
-#closing the socket :
-sc.close()
+try:
+    sc.connect((address, port))
+    print("✈️  ✈️  ✈️   Welcome to aviationstack   ✈️  ✈️  ✈️")
+
+    while True:
+        menu = sc.recv(1024).decode('ascii')
+        print(menu)
+
+        option = input("Enter your choice (a, b, c, or d): ")
+        sc.send(option.encode('ascii'))
+
+        response = sc.recv(65000).decode('ascii')
+        print(response)
+
+        quit = input("Do you want to QUIT (y/n): ")
+        if quit.lower() == 'y':
+            print("Connection has been closed 🔌")
+            print("Good Bye")
+            break
+
+except ConnectionError as e:
+    print(f"Connection error: {e}")
+except KeyboardInterrupt:
+    print("Client interrupted.")
+finally:
+    sc.close()
--- TCP_server.py
+++ TCP_server.py
@@ -1,6 +1,9 @@
 import socket
 import requests
 import json
+import threading
+import sys
+
 
 
 # access code for API
@@ -25,28 +28,96 @@ data = response.json()
 with open('GA6.json', 'w') as f:
         json.dump(data , f , indent=2)
 
-        print("Data saved to GA6.json")
+# creat a TCP socket        
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+address = '127.0.0.2'
+port = 50000
+ss.bind((address, port))
+print('=' * 20 + '  The server has been started ' + '=' * 20)
 
-
-
-
-#creating a TCP socket :
-#ss = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-# binding the socket : 
-#adress ='127.0.0.2'
-#port = 50000
-#ss.bind((adress, port))
-
-# listen for 3 client :
-#ss.listen(3)
+# Handle the client :
+def handle_clients():
     
-#accepting the connection
-#sss, sockname = ss.accept()
+    while True:
+        
+        sss, sockname = ss.accept()
+        print('Accepted request from', sockname[0], 'with port number', sockname[1])
 
-#PRINT THE ACCEPTED METHOD
-#print('Accepted request from', sockname[0] ,'with port number', sockname[1])
+        t = threading.Thread(target=option_C, args=(sss,))
+        my_threads.append(t)
+        t.start()
 
-#closing the socket :
-#ss.close()
-#sss.close()
+        if ss.fileno() == -1:
+            print("Socket is closed.")
+            break
+
+# Listen for clients
+ss.listen()
+
+# option a , b, c, d
+def option_C(sss):
+    try:
+        while True :
+            
+            sss.send(b"Options:\n"
+                    b"a. All arrived flights\n"
+                    b"b. All delayed flights\n"
+                    b"c. All flights from a specific airport\n"
+                    b"d. Details of a particular flight\n"
+                    b"Choose an option (a, b, c, or d): ")
+
+            option = sss.recv(1).decode('ascii')
+
+            with open('GA6.json', 'r') as file:
+                data = json.load(file)
+            listt = ''
+
+            if option.lower() == 'a':
+                
+                for i in range(len(data['data'])):
+                    D = data['data'][i]['arrival']
+                    listt += '#{}: flight IATA code: {} \ndeparture airport name: {} \narrival time: {} \narrival terminal number: {} \narrival gate: {}'.format(
+                        i, D['icao'], D['airport'], D['scheduled'], D['terminal'], D['gate'])
+                    listt += '\n'
+                sss.sendall(listt.encode())
+
+            if option.lower() == 'b':
+                delayed_flights_exist = False
+                for i in range(len(data['data'])):
+                    D = data['data'][i]['departure']
+                    if D["delay"] != None:
+                        listt += '#{}: flight IATA code: {} \ndeparture airport name: {} \narrival time: {} \ndelay:  {} \narrival terminal number: {} \narrival gate: {}'.format(
+                            i, D['icao'], D['airport'], D['scheduled'], D["delay"], D['terminal'], D['gate'])
+                        listt += '\n'
+                        delayed_flights_exist = True
+
+                if not delayed_flights_exist:
+                    listt = 'No delayed flights\n'
+                
+                sss.sendall(listt.encode())
+    except Exception as e:
+        print(f"Error handling client: {e}")
+    finally:
+        sss.close()
+
+     
+
+# threading 
+my_threads = []
+
+try:
+    t = threading.Thread(target=handle_clients)
+    t.start()
+
+    while True:
+        if len(my_threads) >= 5:
+            print('End the server')
+            break
+            
+
+except KeyboardInterrupt:
+    print('Server interrupted. Closing.')
+
+
+ss.close()
+sys.exit()

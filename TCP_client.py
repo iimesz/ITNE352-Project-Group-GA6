42247b14027f17990659c6f23f583a5a76e83574
SalmanNE352 <154358493+SalmanNE352@users.noreply.github.com>
2023-12-25 16:38:38 +0300

    testing client.py

--- TCP_client.py
+++ TCP_client.py
@@ -8,7 +8,49 @@ sc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 adress ='127.0.0.2'
 port = 50000
 sc.connect((adress, port))
-print("The request has been sent")
+print("✈  ✈  ✈   Welcome to aviationstack   ✈  ✈  ✈")
+
+
+try:
+    while True:
+        menu = sc.recv(1024).decode('ascii')
+        print(menu)
+
+        # Choose an option
+        option = input("Enter your choice (a, b, c, or d): ")
+        sc.send(option.encode('ascii'))
+        if option == 'a':
+            IATA= input(' Enter The IATA code')
+            sc.send(IATA.encode('ascii'))
+            response= response = sc.recv(1024).decode('ascii')
+
+        #response = sc.recv(1024).decode('ascii')
+        #print(response)
+
+    
+        another_request = input("Do you want to make another request? (y/n): ")
+        if another_request.lower() != 'y':
+            print(' Thank you ')
+            break
+        
+
+except KeyboardInterrupt:
+    pass
+finally:
+    sc.close()
+
+
+
+
+
+
+
+#print(sc.recv(1024).decode('ascii'))
+#arr_iaco=input('>>>')
+#sc.send(arr_iaco.encode('ascii'))
+#print(sc.recv(1024).decode('ascii'))
+
+
 
 #closing the socket :
 sc.close()
